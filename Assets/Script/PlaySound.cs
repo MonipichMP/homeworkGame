3a0ba3de23c47abc434ed74dc25c3a68c2745a77
Monipich <pichkhmer007@gmail.com>
2019-10-05 23:59:19 +0700

    finish homework1

---
+++ Assets/Script/PlaySound.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlaySound : MonoBehaviour {
+
+    [SerializeField] int timeToWait = 3;
+    int currentIndex;
+
+	// Use this for initialization
+	void Start () {
+        currentIndex = SceneManager.GetActiveScene().buildIndex;
+        if(currentIndex == 0)
+        {
+            StartCoroutine(WaitForTime());
+        }
+	}
+
+    IEnumerator WaitForTime()
+    {
+        yield return new WaitForSeconds(timeToWait);
+        LoadNextScene();
+    }
+	
+    public void LoadNextScene()
+    {
+        SceneManager.LoadScene(currentIndex + 1 );
+    }
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
